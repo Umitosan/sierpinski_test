6ef06cc190c3b3d04b4d82102a7ec185cb2b02be
Umitosan <yeahdom@gmail.com>
2019-03-26 20:51:51 -0700

    boil it up

---
+++ scripts/helpers.js
@@ -0,0 +1,22 @@
+/*jshint esversion: 6 */
+
+
+function clearCanvas() {
+  ctx.clearRect(-1, -1, canvas.width+1, canvas.height+1); // offset by 1 px because the whole canvas is offset initially (for better pixel accuracy)
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
+function generalLoopReset() {
+  if (State.myReq !== undefined) {  // reset game loop if already started
+    cancelAnimationFrame(State.myReq);
+  }
+  softReset();
+  myGame = new Game(State.simSpeed); // ms per update()
+  myGame.init();
+  State.myReq = requestAnimationFrame(gameLoop);
+}
